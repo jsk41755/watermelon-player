ee4f3646149df1d1e42526707b046d16fee6f0db
JeongSG <jsk41755@naver.com>
2023-12-03 00:35:06 +0900

    !BREAKING CHANGE: gradle 구조 변경 8

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -72,10 +72,6 @@ kotlin {
         val iosArm64Test by getting
         val iosSimulatorArm64Test by getting
         val iosTest by getting {
-            dependsOn(commonTest)
-            iosX64Test.dependsOn(this)
-            iosArm64Test.dependsOn(this)
-            iosSimulatorArm64Test.dependsOn(this)
         }
     }
 }
