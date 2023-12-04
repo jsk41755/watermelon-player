b9a407ea34dd501c185ead99cce13c340e355929
JeongSG <jsk41755@naver.com>
2023-12-05 00:44:29 +0900

    feat: Added flow(stateFlow) to commonMain

---
+++ shared/src/commonMain/kotlin/com/devjeong/watermelon_player/core/domain/util/CommonMutableStateFlow.kt
@@ -0,0 +1,9 @@
+package com.devjeong.watermelon_player.core.domain.util
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+expect class CommonMutableStateFlow<T>(flow: MutableStateFlow<T>): MutableStateFlow<T>
+
+fun <T> MutableStateFlow<T>.toCommonMutableStateFlow() = CommonMutableStateFlow(this)
---
+++ shared/src/commonMain/kotlin/com/devjeong/watermelon_player/core/domain/util/Resource.kt
@@ -0,0 +1,6 @@
+package com.devjeong.watermelon_player.core.domain.util
+
+sealed class Resource<T>(val data: T?, val throwable: Throwable? = null) {
+    class Success<T>(data: T): Resource<T>(data)
+    class Error<T>(throwable: Throwable): Resource<T>(null, throwable)
+}
