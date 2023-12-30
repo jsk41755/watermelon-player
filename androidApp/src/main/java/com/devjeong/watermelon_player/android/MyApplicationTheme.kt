2caa2325e9d92043f9ef45db1db7e22d29007590
JeongSG <jsk41755@naver.com>
2023-12-30 18:34:22 +0900

    폰트 및 statusBar 색상 변경

--- androidApp/src/main/java/com/devjeong/watermelon_player/android/MyApplicationTheme.kt
+++ androidApp/src/main/java/com/devjeong/watermelon_player/android/MyApplicationTheme.kt
@@ -1,5 +1,6 @@
 package com.devjeong.watermelon_player.android
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
@@ -8,12 +9,17 @@ import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.view.WindowCompat
 
 @Composable
 fun MyApplicationTheme(
@@ -37,7 +43,7 @@ fun MyApplicationTheme(
     }
     val typography = Typography(
         bodyMedium = TextStyle(
-            fontFamily = FontFamily.Default,
+            fontFamily = FontFamily(Font(R.font.pretendard)),
             fontWeight = FontWeight.Normal,
             fontSize = 16.sp
         )
@@ -47,6 +53,15 @@ fun MyApplicationTheme(
         medium = RoundedCornerShape(4.dp),
         large = RoundedCornerShape(0.dp)
     )
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = Color(0xFF151515).toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
+        }
+    }
 
     MaterialTheme(
         colorScheme = colors,
