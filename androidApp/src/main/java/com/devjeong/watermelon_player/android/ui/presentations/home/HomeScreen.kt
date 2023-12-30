90e548b15efab1dcd041bf21490ea1cf7991a8f9
JeongSG <jsk41755@naver.com>
2023-12-30 23:36:17 +0900

    각 페이지 디자인 수정

--- androidApp/src/main/java/com/devjeong/watermelon_player/android/ui/presentations/home/HomeScreen.kt
+++ androidApp/src/main/java/com/devjeong/watermelon_player/android/ui/presentations/home/HomeScreen.kt
@@ -1,20 +1,34 @@
 package com.devjeong.watermelon_player.android.ui.presentations.home
 
+import android.app.Activity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.devjeong.watermelon_player.android.MyApplicationTheme
 
 @Composable
 fun HomeScreen() {
+    val context = LocalContext.current
+    val window = (context as Activity).window
+
+    SideEffect {
+        window.statusBarColor = Color(0xFF151515).toArgb()
+        WindowCompat.getInsetsController(window, window.decorView)
+            .isAppearanceLightStatusBars = false
+    }
+
     Box(
         modifier = Modifier
             .fillMaxSize()
--- androidApp/src/main/java/com/devjeong/watermelon_player/android/ui/presentations/like/LikeScreen.kt
+++ androidApp/src/main/java/com/devjeong/watermelon_player/android/ui/presentations/like/LikeScreen.kt
@@ -1,21 +1,47 @@
 package com.devjeong.watermelon_player.android.ui.presentations.like
 
+import android.app.Activity
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.view.WindowCompat
 import androidx.navigation.NavController
 import com.devjeong.watermelon_player.android.ui.presentations.playlist.PlayListViewModel
+import com.devjeong.watermelon_player.android.ui.presentations.playlist.components.NowPlayMusic
 import com.devjeong.watermelon_player.android.ui.presentations.playlist.components.SongItem
 
 @Composable
 fun LikeScreen(navController: NavController, playListViewModel: PlayListViewModel) {
+    val backgroundColor = remember {
+        Color(0xFFDBDBDB)
+    }
+
+    val context = LocalContext.current
+    val window = (context as Activity).window
+
+    SideEffect {
+        window.statusBarColor = backgroundColor.toArgb()
+        WindowCompat.getInsetsController(window, window.decorView)
+            .isAppearanceLightStatusBars = true
+    }
+
     val likedMusicList by playListViewModel.musicListViewModel.likedMusicList.collectAsState()
 
-    LazyColumn {
-        items(items = likedMusicList.sortedBy { it.id }) { music ->
-            SongItem(navController = navController, music = music)
+    Column {
+        NowPlayMusic(backgroundColor)
+
+        LazyColumn {
+            items(items = likedMusicList.sortedBy { it.id }) { music ->
+                SongItem(navController = navController, music = music)
+            }
         }
     }
 }
